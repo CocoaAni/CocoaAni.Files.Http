1cf44831f87ae6784c3601a56905c7a2cd60f3cd
lzy-0213 <lzy-0213@outlook.com>
2023-02-02 13:44:46 +0800

    添加项目文件。

---
+++ HttpFiles.cs
@@ -0,0 +1,21 @@
+﻿namespace CocoaAni.Files.Http;
+
+public class HttpFile
+{
+    public string NetUrl { get; }
+    public string LocalPath { get; }
+
+    public FileInfo Info { get; }
+
+    public HttpFile(string localPath, string netUrl)
+    {
+        LocalPath = localPath;
+        NetUrl = netUrl;
+        Info = new FileInfo(localPath);
+    }
+
+    public HttpFile(FileInfo fileInfo, string netUrl) : this(fileInfo.FullName, netUrl)
+    {
+        Info = fileInfo;
+    }
+}
